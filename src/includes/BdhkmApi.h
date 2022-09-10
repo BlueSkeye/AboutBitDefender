12408ef93e75dee00cabe6f75832becb103b29fb
BlueSkeye <M2438FE89@gmail.com>
2022-09-10 17:53:34 +0200

    ADDED : BdhkmResolveInlineFunctionAddress

--- src/includes/BdhkmApi.h
+++ src/includes/BdhkmApi.h
@@ -34,6 +34,8 @@ UNEXPLORED BdhkmRemovePointerHook
 * hModule : Handle to the module to be scanned for imports.
 * dllName : The name of the DLL exporting the artefact.
 * exportName : Artefact's name
+* ppImageImportByName : Pointer at a location that on successfull return will contain
+*    the descriptor of the searched artefact.
 * Returns : TRUE on successfull retrieval, FALSE otherwise.
 * REMARKS : There is no validation of the hModule value itself. However the retrieval
 * of the exported artefact is tightly bound to the official PE format. Attempt to use
@@ -46,7 +48,23 @@ BDHKMAPI __fastcall BOOL BdhkmResolveImportAddress(
 	_In_ PSTRING exportName,
 	_Inout_ PIMAGE_IMPORT_BY_NAME* ppImageImportByName);
 
-UNEXPLORED BdhkmResolveInlineFunctionAddress
+/* Retrieve the in memory address of the function having the given name in the module
+* loaded at the given address.
+* hModule : Handle to the module to be scanned for imports.
+* functionName : The name of the searched function.
+* ppProcedureAddress : Pointer at a location that on successfull return will contain
+*    the in memory address of the searched function.
+* Returns : TRUE on successfull retrieval, FALSE otherwise.
+* REMARKS : There is no validation of the hModule value itself. However the retrieval
+* of the exported artefact is tightly bound to the official PE format. Attempt to use
+* a non legit hModule value would very likely fail. Should it succeed it is not
+* expected to provide any advantage or bypass.
+*/
+BDHKMAPI __fastcall BOOL BdhkmResolveInlineFunctionAddress(
+	_In_ HMODULE hModule,
+	_In_ STRING functionName,
+	_Inout_ PVOID* ppProcedureAddress);
+
 UNEXPLORED BdhkmResolveRvaExportTableAddress
 UNEXPLORED BdhkmResumeEatHook
 UNEXPLORED BdhkmResumeIatHook
